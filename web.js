5c8c6bca6c0a479c93fbdf23c808bf928d16cf7f
Timothy J. Sayre <tsayre@cachehive.com>
2014-03-18 14:53:43 -0700

    added page frame and css mark-up.

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var infile = "index.html";
+var content = fs.readFileSync( infile );
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send( content.toString() + '\n');
 });
 
 var port = process.env.PORT || 5000;
